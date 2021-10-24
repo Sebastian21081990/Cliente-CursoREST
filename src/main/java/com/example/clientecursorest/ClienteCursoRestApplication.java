7b5615ad9ee5fecda5a57847814fd62761dafb33
david <sebastianvivanco21081990@gmail.com>
2021-10-23 20:08:26 -0500

    CLiente Curso-REST

---
+++ src/main/java/com/example/clientecursorest/ClienteCursoRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientecursorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteCursoRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteCursoRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clientecursorest/ClienteCursoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientecursorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteCursoRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
